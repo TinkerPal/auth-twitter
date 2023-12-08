338908386c60865ffca6e71850ee8cea46f780af
TinkerPal <tinkerpi2023@gmail.com>
2023-12-08 09:09:41 -0500

    update

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "*", // allow to server to accept request from different origin
+    origin: "https://app.audaxious.com/", // allow to server to accept request from different origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
   })
